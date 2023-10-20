12d31755450fcddb95ad7cb2eecf2836c08d6f34
Afanas2707 <avg57327@gmail.com>
2023-10-20 22:20:59 +0300

    stl

---
+++ stl.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <string>
+#include <list>
+
+int main() {
+
+    setlocale(LC_ALL, "Russian");
+
+    std::string input;
+    std::cout << "Введите строку: ";
+    std::getline(std::cin, input);
+
+    std::list<char> charList;
+
+    // Заполняем список символами из введенной строки
+    for (char c : input) {
+        charList.push_front(c);
+    }
+
+    // Выводим символы в обратной последовательности
+    std::cout << "Строка в обратной последовательности: ";
+    for (char c : charList) {
+        std::cout << c;
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
