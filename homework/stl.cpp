1901cac2b68ea5a01cb94898dd0291c9b3b5f3dd
Afanas2707 <127185135+Afanas2707@users.noreply.github.com>
2023-10-20 23:12:12 +0300

    Update stl.cpp

--- homework/stl.cpp
+++ homework/stl.cpp
@@ -1,10 +1,14 @@
-﻿#include <iostream>
+#include <iostream>
 #include <string>
 #include <list>
+#include "windows.h"
 
 int main() {
 
     setlocale(LC_ALL, "Russian");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
 
     std::string input;
     std::cout << "Введите строку: ";
@@ -16,6 +20,8 @@ int main() {
     for (char c : input) {
         charList.push_front(c);
     }
+
+    SetConsoleCP(1251);
 
     // Выводим символы в обратной последовательности
     std::cout << "Строка в обратной последовательности: ";
